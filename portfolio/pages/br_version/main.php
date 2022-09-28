bed63268049059172417af90d6a17abd0cef9704
Luiz Gustavo Soares do Nascimento <55930698+vaiBEAR@users.noreply.github.com>
2022-09-28 13:39:30 -0300

    Add files via upload

---
+++ portfolio/pages/br_version/main.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/teste.css">
+    <script>
+        src = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    </script>
+    <title>LgPortfolio_br</title>
+    <link rel="shortcut icon" href="img/lg_logo.png">
+</head>
+
+<body>
+
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="container mt-5">
+                <div class="mt-1 p-5 bg-primary text-white rounded">
+                    <div class="far">
+                        <h4>vaiLG#7004 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-patch-check" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10.354 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                                <path d="m10.273 2.513-.921-.944.715-.698.622.637.89-.011a2.89 2.89 0 0 1 2.924 2.924l-.01.89.636.622a2.89 2.89 0 0 1 0 4.134l-.637.622.011.89a2.89 2.89 0 0 1-2.924 2.924l-.89-.01-.622.636a2.89 2.89 0 0 1-4.134 0l-.622-.637-.89.011a2.89 2.89 0 0 1-2.924-2.924l.01-.89-.636-.622a2.89 2.89 0 0 1 0-4.134l.637-.622-.011-.89a2.89 2.89 0 0 1 2.924-2.924l.89.01.622-.636a2.89 2.89 0 0 1 4.134 0l-.715.698a1.89 1.89 0 0 0-2.704 0l-.92.944-1.32-.016a1.89 1.89 0 0 0-1.911 1.912l.016 1.318-.944.921a1.89 1.89 0 0 0 0 2.704l.944.92-.016 1.32a1.89 1.89 0 0 0 1.912 1.911l1.318-.016.921.944a1.89 1.89 0 0 0 2.704 0l.92-.944 1.32.016a1.89 1.89 0 0 0 1.911-1.912l-.016-1.318.944-.921a1.89 1.89 0 0 0 0-2.704l-.944-.92.016-1.32a1.89 1.89 0 0 0-1.912-1.911l-1.318.016z" />
+                            </svg>
+                            <a href="/portfolio">
+                                <div class="img-container btn" id="altair">
+                                    <img src="img/reino-unido.png" height="30" id="uk" alt="//https://www.flaticon.com/br/icones-gratis/inglaterra">
+                                </div>
+                        </h4>
+                    </div>
+                    </a>
+                    </br>
+                    <i class="bi bi-patch-check"></i>
+                    <div class="img-container">
+                        <img src="img/me.jpg" alt="it should be me :(" class="rounded-circle" width="150" height="150">
+                        </br></br>
+                    </div>
+                    <p>Ola! Me chamo Luiz Gustavo, Tenho 18 anos e vivo em São Paulo. Atualmente estou focado em
+                        conseguir minha primeira oportunidade na área de programação (front-end ou back-end)</p>
+
+                    <a role="button" target="_blank" class="btn btn-dark btn-sm" href="https://github.com/vaiBEAR"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                        </svg>
+                    </a>
+
+                    <a role="button" target="_blank" class="btn btn-dark btn-sm" href="https://www.linkedin.com/in/luiz-gustavo-soares-do-nascimento-1b598a203/">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                        </svg>
+                    </a>
+
+                    <a role="button" target="_blank" class="btn btn-dark btn-sm" href="https://api.whatsapp.com/send?phone=5511945299839&text=Ol%C3%A1%2C%20vamos%20trabalhar%20juntos%20%3F">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+                            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            </br></br>
+            <div class="text-center">
+                <h3 id="interesting" class="text-white">Pode ser interessante...</h3></br></br>
+
+                <a href="paginas/hab/habilidades.php">
+                    <div class="col-sm-3 col-md-6 text-white rounded bg-primary button btn">
+                        <div class="img-container">
+                            <img src="img/certificado.png" alt="https://www.flaticon.com/br/icones-gratis/certificado" height="60">
+                            <h5>HABILIDADES !!!</h5>
+                            Dê uma olhada nas minhas competências
+                        </div>
+
+                    </div>
+                </a>
+
+
+                </br></br>
+
+
+                <a href="sobre_mim/sobre_eu.php">
+                    <div class="col-sm-3 col-md-6 text-white rounded bg-primary button btn">
+                        <div class="img-container">
+                            <img src="img/know_me.png" alt="know me icon" height="60">
+                            <h5>Conheça um pouco da minha história</h5>
+                            Clique aqui para ver um pouco da minha vida
+                        </div>
+
+                    </div>
+                </a>
+
+                </br></br>
+
+                <a href="contato/contato.php">
+                    <div class="col-sm-3 col-md-6 text-white rounded bg-primary button btn">
+                        <div class="img-container">
+                            <img src="img/contato.png" alt="https://www.flaticon.com/br/icones-gratis/contato" height="60">
+                            <h5>ENTRE EM CONTATO</h5>
+                            Vamos trabalhar juntos !
+                        </div>
+
+                    </div>
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+    </br></br>
+
+</body>
+
+</html>
